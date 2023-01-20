663e5f6af0e7e9efd8624ebd596e7eea9ab7c1ef
batuklambu <batuklambu@gmail.com>
2023-01-20 12:07:55 -0500

    dayweek7day3lab

---
+++ week7day3lab.js
@@ -0,0 +1,30 @@
+// What is a Single Page Application?
+// UI/UX: Single Page Applications (SPA) are a great way to create a fantastic user experience on mobile devices.
+// They consist of a single web page that loads all content using JavaScript. They request the markup and data independently and render pages straight to the browser.
+// Reloading: Single-page applications are web pages that only require a single HTML page to load.
+// They are generally used to perform actions that would take too long to load via traditional web pages, such as downloading files or rendering images.
+// A single-page application comprises HTML and JavaScript, usually including a server-side language (such as PHP) that processes data and returns the results.
+
+// What is REACT?
+//React is a JavaScript library for building user interfaces
+
+// What is JSX?
+//  A syntax which is neither a string nor HTML is Jsx.it is a syntax extension to JavaScript
+// for e.g.const element = <h1>Hello, world!</h1>;
+
+// What is Babel?
+// Babel is a JavaScript compiler.
+// Babel Transform syntax.Polyfill features that are missing in your target environment (through a third-party polyfill such as core-js)
+// Source code transformations (codemods)And more!.Babel can convert JSX syntax! Check out our React preset to get started.
+// Use it together with the babel-sublime package to bring syntax highlighting to a whole new level.
+
+// What is Webpack?
+//webpack is a static module bundler for modern JavaScript applications.
+//When webpack processes your application, it internally builds a dependency graph
+//from one or more entry points and then combines every module your project needs into one or more bundles,
+//which are static assets to serve your content from.
+
+// What is Create React App?
+//Create React App is a comfortable environment for learning React, and is the best way to start building a new single-page application in React.
+//It sets up your development environment so that you can use the latest JavaScript features,
+//provides a nice developer experience, and optimizes your app for production.
